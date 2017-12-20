f4a04c867959b83b3d5b586c409334885b5c960d
Haseeb Ehsan <haseeb.ahmad57@gmail.com>
2017-12-20 10:36:23 +0000

    First commit

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>@yield('title')</title>
+        <link rel="stylesheet" href="{{ URL::secure('src/css/main.css') }}" >
+        @yield('styles')
+    </head>
+    <body>
+        <div class="main">
+            @yield('content')
+        </div>
+    </body>
+</html>
